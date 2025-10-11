50ebde276e992c5b666d6ae1f4f1ecb7c014b769
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-10-12 00:50:43 +0300

    create app urls class

---
+++ lib/core/constants/app_urls.dart
@@ -0,0 +1,14 @@
+
+class AppUrls {
+  static const String onboardingImage = 'https://picsum.photos/400/250?random=1';
+  static const String dumbbellPressLarge = 'https://picsum.photos/600/400?random=2';
+
+  // Exercise Thumbnails
+  static const String benchPressThumb = 'https://picsum.photos/100/100?random=3';
+  static const String dumbbellPressThumb = 'https://picsum.photos/100/100?random=4';
+  static const String cableMachineThumb = 'https://picsum.photos/100/100?random=5';
+  static const String shoulderPressThumb = 'https://picsum.photos/100/100?random=6';
+  static const String tricepExtensionThumb = 'https://picsum.photos/100/100?random=7';
+  static const String chestFlyThumb = 'https://picsum.photos/100/100?random=8';
+  static const String lateralRaiseThumb = 'https://picsum.photos/100/100?random=9';
+}
