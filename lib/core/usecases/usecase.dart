5411655e669ca45d678ac811e5d46f34749c2f4f
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-10-12 01:01:04 +0300

    create use case class and add dartz package

---
+++ lib/core/usecases/usecase.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import '../error/failures.dart';
+
+// T = return type (e.g., List<Entity>, void)
+// P = parameters type (e.g., String, Entity, NoParams)
+
+abstract class UseCase<T, P> {
+  Future<Either<Failure, T>> call([P? params]);
+}
+
+// Used for use cases that do not require any parameters
+
+class NoParams {
+  const NoParams();
+}
