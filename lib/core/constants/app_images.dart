c376898c813e4ec953254037676b3855185e5655
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-10-12 00:51:02 +0300

    create app images class

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,12 @@
+// We will use network URLs instead of local assets where appropriate,
+// but keep this file for structural integrity.
+
+class AppImages {
+  static const String logo = 'assets/images/logo.png';
+
+  // Dashboard Icons (Placeholder for actual SVG/PNG)
+  static const String iconGym = 'assets/icons/gym.svg';
+  static const String iconCardio = 'assets/icons/cardio.svg';
+  static const String iconLinks = 'assets/icons/links.svg';
+  static const String iconFoods = 'assets/icons/foods.svg';
+}
