8b9fafb4666e9c725ed4854d7a1017ac3ee4bf5e
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-10-17 19:24:42 +0300

    create and implement on boarding view body

--- lib/features/onboarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/onboarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,14 +1,73 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:iron_counter/core/constants/app_colors.dart';
+import 'package:iron_counter/core/constants/app_strings.dart';
+import 'package:iron_counter/core/constants/app_urls.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
   const OnBoardingViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
+    return Padding(
+      padding: EdgeInsets.all(24.w),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Spacer(flex: 2),
+          Center(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16.r),
+              child: CachedNetworkImage(
+                imageUrl: AppUrls.onboardingImage,
+                fit: BoxFit.cover,
+                height: 200.r,
+                placeholder: (context, url) => Container(
+                  height: 200.r,
+                  color: AppColors.surface,
+                  child: Center(
+                    child: const CircularProgressIndicator(
+                      color: AppColors.primary,
+                    ),
+                  ),
+                ),
+                errorWidget: (context, url, error) => Icon(
+                  Icons.fitness_center,
+                  size: 200.w,
+                  color: AppColors.primary,
+                ),
+              ),
+            ),
+          ),
+          const Spacer(flex: 1),
 
-      ],
+          Text(
+            AppStrings.onboardingTitle,
+            style: Theme.of(context).textTheme.titleLarge?.copyWith(
+              color: AppColors.primary,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 16.r),
+
+          Text(
+            AppStrings.onboardingSubtitle,
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
+          const Spacer(flex: 3),
+
+          ElevatedButton(
+            onPressed: () {
+
+            },
+            child: Text(AppStrings.onboardingButton),
+          ),
+          SizedBox(height: 20.r),
+
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:iron_counter/core/helper_functions/on_generate_routes.dart';
+import 'package:iron_counter/core/theme/app_theme.dart';
 import 'package:iron_counter/features/splash/presentation/views/splash_view.dart';
 
 void main() {
@@ -11,10 +13,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: onGenerateRoutes,
-      initialRoute: SplashView.routeName,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          theme: AppTheme.darkTheme,
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: onGenerateRoutes,
+          initialRoute: SplashView.routeName,
+        );
+      },
     );
   }
 }
