202b0e325bc8369f9673e676063406f590a0f6a6
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-10-25 18:01:09 +0300

    integrate general use case class in get dashboard data usecase class

--- lib/features/dashboard/domain/usecases/get_dashboard_data_usecase.dart
+++ lib/features/dashboard/domain/usecases/get_dashboard_data_usecase.dart
@@ -1,14 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:training_sync/core/error/failure.dart';
+import 'package:training_sync/core/usecases/usecase.dart';
 import 'package:training_sync/features/dashboard/domain/entities/dashboard_entity.dart';
 import 'package:training_sync/features/dashboard/domain/repos/dashboard_repo.dart';
 
-class GetDashboardDataUseCase {
+class GetDashboardDataUseCase extends UseCase<List<DashboardEntity>, NoParams> {
   GetDashboardDataUseCase(this.dashboardRepo);
 
   final DashboardRepo dashboardRepo;
 
-  Future<Either<Failure, List<DashboardEntity>>> call() async {
+  @override
+  Future<Either<Failure, List<DashboardEntity>>> call([NoParams? params]) async{
     return await dashboardRepo.getDashboardCategories();
   }
 }
